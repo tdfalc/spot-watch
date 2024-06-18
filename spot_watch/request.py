04f4796b119d44929d781e40b57d5acd52761824
tdfalc <tomdfalc@gmail.com>
2024-06-18 08:23:24 +0200

    Remove print statement

--- spot_watch/request.py
+++ spot_watch/request.py
@@ -46,9 +46,8 @@ def request_day_ahead_auction_results(
     columns = {"Volume(MWh)": "volume", "Price(£/MWh)": "price"}
 
     df = pd.read_html(html)[0]
-    df.columns = [col.replace(" ", "") for col in df.columns]
     df.columns = df.columns.get_level_values(-1)
-    print(df.columns)
+    df.columns = [col.replace(" ", "") for col in df.columns]
     df = df.loc[:, columns.keys()].rename(columns=columns)
     df.convert_dtypes()
 
